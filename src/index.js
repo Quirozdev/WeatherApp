cfca9364452d102792ab33575ba278ab386b84a4
Luis <luisdevv232@gmail.com>
2023-07-18 23:10:57 -0700

    Added webpack to the project

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import data from './weather_conditions.json';
+import '../public/style.css';
+
+console.log(data);
+
+const form = document.querySelector('form');
+const locationInput = document.getElementById('location');
+const infoDisplayer = document.querySelector('.info-displayer');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const location = locationInput.value;
+  const weatherData = await getWeatherData(location);
+  console.log(weatherData);
+  fillDOMContent(weatherData);
+});
+
+async function getWeatherData(country) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=33cf022bfd764080a3831303231907&q=${country}&aqi=no`
+  );
+  const weatherData = await response.json();
+  return weatherData;
+}
+
+const locationElem = document.querySelector('.location-info');
+const humidityElem = document.querySelector('.humidity');
+const temperatureElem = document.querySelector('.temperature');
+const temperatureConditionElem = document.querySelector(
+  '.temperature-condition'
+);
+const weatherIconElem = document.getElementById('weather-icon');
+
+function fillDOMContent({ current, location }) {
+  locationElem.textContent = `${location.name}, ${location.region}, ${location.country}`;
+  humidityElem.textContent = `${current.humidity}%`;
+  temperatureElem.textContent = `${current.feelslike_c}° C`;
+  temperatureConditionElem.textContent = `Condition: ${current.condition.text}`;
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+  },
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public/resources', to: 'resources' }],
+    }),
+  ],
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
